3cb7f3d0010c2206e81c27fe5053b41cae6d6131
shenzhuoyi <Zhuoyi.SHEN20@student.xjtlu.edu.cn>
2023-04-11 13:54:21 +0800

    m

--- src/main/java/com/example/pizza_order_demo/service/UserAddressService.java
+++ src/main/java/com/example/pizza_order_demo/service/UserAddressService.java
@@ -2,8 +2,9 @@ package com.example.pizza_order_demo.service;
 
 
 import com.example.pizza_order_demo.model.UserAddress;
+import com.example.pizza_order_demo.model.UserAddressExample;
 
-public interface UserAddressService  {
+public interface UserAddressService  extends BaseService<UserAddress, UserAddressExample>{
 
 
 
